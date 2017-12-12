3c86b3df04ee5e6b1a550d470858bd5cf4d1c13c
d3qnew <d3qnew@163.com>
2017-12-12 16:55:22 +0800

    001

--- static/js/myjs/myjs.js
+++ static/js/myjs/myjs.js
@@ -2,3 +2,7 @@
  * Created by d3qnew on 2017/11/20.
  */
 
+$(document).ready(function () {
+
+
+});
--- static/js/myjs/navbar.open.js
+++ static/js/myjs/navbar.open.js
@@ -12,19 +12,36 @@ $(document).ready(function () {
         }
     });
 
+
+    if ($(window).width() < 992) {
+        notx992();
+    } else {
+        tx992();
+    }
+    $(window).resize(function () {
+        if ($(window).width() < 992) {
+            notx992();
+        } else {
+            tx992();
+        }
+    });
+
+
     function notx() {
 
         $(".navbaropen").off();
         $(".navbaropen").removeClass("navbar-titel-border navbar-titel-borderx");
         $(".navbaropen").first().css("color", "#e50000");
+        $(".navbaropen").css("margin-left","0");
         $(".navbaropen + ul div div").removeClass("navbar-top-line");
-        $(".navbaropen").addClass("navbaropen-min");
+        //$(".navbaropen").addClass("navbaropen-min");
         $(".tonavmenuls li a").addClass("navmenuls-min");
         $(".tonavmenuls").parent().each(function () {
-            $(this).css("border-bottom","");
+            $(this).css("border-bottom", "");
         });
         $("#example-navbar-collapse ul").addClass("toborderbottom");
         //$(".navbaropen ").parent().css("border-bottom","1px solid #ddd");
+        $("")
 
 
     }
@@ -36,7 +53,8 @@ $(document).ready(function () {
         $(".tonavmenuls").css("border-bottom", "0px");
         $(".tonavmenuls > li > a").removeClass("navmenuls-min");
         $(".navbaropen + ul div div").addClass("navbar-top-line");
-        $(".navbaropen").removeClass("navbaropen-min");
+        $(".navbaropen").css("margin-left","30px");
+        //$(".navbaropen").removeClass("navbaropen-min");
 
 
         //$(".navbaropen").first().addClass("navbar-titel-borderx");
@@ -78,66 +96,76 @@ $(document).ready(function () {
 
     }
 
+
+    function tx992() {
+        $(".navbar-brand").css({"width":"248px"});
+    }
+
+    function notx992() {
+        $(".navbar-brand").css({"width":"64px", "overflow":"hidden"});
+    }
+
+
     $(".totopauto li").addClass("col-xs-12 col-sm-12 col-md-6 col-lg-4");
 
     /*
-        $(".navbaropen").mouseenter(function () {
-            var oldcolor = $(this).css("color");
-            tcolor($(this),"#f00000");
-            $(this).mouseleave(function () {
-                tcolor($(this),oldcolor);
-            })
-        });
+     $(".navbaropen").mouseenter(function () {
+     var oldcolor = $(this).css("color");
+     tcolor($(this),"#f00000");
+     $(this).mouseleave(function () {
+     tcolor($(this),oldcolor);
+     })
+     });
 
 
-        function tcolor(el, color) {
-            var c1 = el.css('color');
-            //console.log(c1);
-            var c2 = gradient(c1, color, 3);
-            //console.log(c2);
-            for(var i in c2){
-                setTimeout(function () {
-                    el.css("color",c2[i]);
-                },200);
-            }
+     function tcolor(el, color) {
+     var c1 = el.css('color');
+     //console.log(c1);
+     var c2 = gradient(c1, color, 3);
+     //console.log(c2);
+     for(var i in c2){
+     setTimeout(function () {
+     el.css("color",c2[i]);
+     },200);
+     }
 
 
 
-        }
+     }
 
 
-        function rgbToHex(r, g, b) {
-            var hex = ((r << 16) | (g << 8) | b).toString(16);
-            return "#" + new Array(Math.abs(hex.length - 7)).join("0") + hex;
-        }
+     function rgbToHex(r, g, b) {
+     var hex = ((r << 16) | (g << 8) | b).toString(16);
+     return "#" + new Array(Math.abs(hex.length - 7)).join("0") + hex;
+     }
 
-        function hexToRgb(hex) {
-            var rgb = [];
-            for (var i = 1; i < 7; i += 2) {
-                rgb.push(parseInt("0x" + hex.slice(i, i + 2)));
-            }
-            return rgb;
-        }
+     function hexToRgb(hex) {
+     var rgb = [];
+     for (var i = 1; i < 7; i += 2) {
+     rgb.push(parseInt("0x" + hex.slice(i, i + 2)));
+     }
+     return rgb;
+     }
 
 
-        function gradient(startColor, endColor, step) {
-            //将hex转换为rgb
-            var sColor = startColor,
-                eColor = endColor;
-            //console.log(sColor,eColor);
-            //计算R\G\B每一步的差值
-            var rStep = (eColor[0] - sColor[0]) / step;
-            gStep = (eColor[1] - sColor[1]) / step;
-            bStep = (eColor[2] - sColor[2]) / step;
+     function gradient(startColor, endColor, step) {
+     //将hex转换为rgb
+     var sColor = startColor,
+     eColor = endColor;
+     //console.log(sColor,eColor);
+     //计算R\G\B每一步的差值
+     var rStep = (eColor[0] - sColor[0]) / step;
+     gStep = (eColor[1] - sColor[1]) / step;
+     bStep = (eColor[2] - sColor[2]) / step;
 
-            var gradientColorArr = [];
-            for (var i = 0; i < step; i++) {
-                //计算每一步的hex值
-                gradientColorArr.push(rgbToHex(parseInt(rStep * i + sColor[0]), parseInt(gStep * i + sColor[1]), parseInt(bStep * i + sColor[2])));
-            }
-            return gradientColorArr;
-        }
-    */
+     var gradientColorArr = [];
+     for (var i = 0; i < step; i++) {
+     //计算每一步的hex值
+     gradientColorArr.push(rgbToHex(parseInt(rStep * i + sColor[0]), parseInt(gStep * i + sColor[1]), parseInt(bStep * i + sColor[2])));
+     }
+     return gradientColorArr;
+     }
+     */
 
 
 });
